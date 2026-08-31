4ed2633ed70de7e0f3be93e4002fa40043b30528
Oleg Adamov <103196379+OlegAdamov@users.noreply.github.com>
2026-08-31 11:40:53 +0300

    Correcting code with task

--- goit-algo-hw-04-03/homework_task_3.py
+++ goit-algo-hw-04-03/homework_task_3.py
@@ -1,27 +1,66 @@
 
-from colorama import colorama_text, just_fix_windows_console
+from colorama import just_fix_windows_console, Fore, Back, Style, init
 from pathlib import Path
 import sys
+init(autoreset=True)
+Style.RESET_ALL
 
 
-if len(sys.argv) < 0:
-    user_input = ''
-else:
-    user_input = sys.argv[1]
-parent_folder_path = Path(user_input)
-# print(path)
 
 
-def parse_folder(path):
-    for element in path.iterdir():
-        if element.is_dir():
-            print(f"Parse folder: This is a folder - {element.name}")
-        if element.is_file():
-            print(f"Parsle folder: This is a file - {element.name}")
+def parse_folder_recursive(path):
+    
+    path_obj = path
+    if not path_obj.exists() or not path_obj.is_dir():  # перевірки шляхів та папки
+        return
+    
+    print(path_obj)
+    def dfs_walk(current_dir, index_slash=0, old_margin=''):
+        
+        for item in current_dir.iterdir():  # iterdir() зчитує вміст поточної папки
+            # print(f"{item}", type(item))
+            item_string = str(item)
+            # print(f"{item_string = }")
+
+            index_slash = item_string.find('\\', index_slash+1)     # знаходимо індекси слешів для обрізки шляху
+            # print(f"{index_slash = }", type(index_slash))
+
+            # Обрізаємо шлях до назви файлу чи папки
+            margin = len(current_dir.as_posix()) - len(old_margin) - 1
+            margin_item = ' '
+            # old_margin = ' ' * len(old_margin)
+            margin = old_margin + ' ' + margin * margin_item
+            
+            for_repl = current_dir.as_posix()
+            clean_path = item.as_posix().replace(f"{for_repl}/", margin)
+            print(clean_path)
+            
+            if item.is_dir():   # Якщо це папка, пірнаємо на глибину
+                dfs_walk(item, index_slash, margin)
+
+    dfs_walk(path_obj)
+
+
 
 
 def main():
-    parse_folder(parent_folder_path)
+
+    if len(sys.argv) < 2:
+        print('Folder is not found')
+        return
+    else:
+        file_name = Path(sys.argv[1])
+    # print(f"{file_name = }")
+
+    parent_folder_path = Path(file_name)
+    # print(f"{parent_folder_path = }")
+
+    parse_folder_recursive(parent_folder_path)
+
+
+    # print(path)
+
+
 
 
 if __name__ == '__main__':
---
+++ goit-algo-hw-04-03/test.py
@@ -0,0 +1,16 @@
+from colorama import just_fix_windows_console, Fore, Back, Style, init
+init(autoreset=True)
+Style.RESET_ALL
+
+
+print(Fore.RED + 'hello')
+print('hello')
+
+print(Fore.BLUE + 'hello')
+print('hello')
+
+print(Fore.CYAN + 'hello')
+print('hello')
+
+print(Fore.GREEN + 'hello')
+print('hello')
