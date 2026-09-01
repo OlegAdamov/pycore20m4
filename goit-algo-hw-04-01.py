d6ed48838d15ec40f020f36f795655aac08dbc12
Oleg Adamov <103196379+OlegAdamov@users.noreply.github.com>
2026-09-01 12:32:11 +0300

    code wrapped in a function

--- goit-algo-hw-04-01.py
+++ goit-algo-hw-04-01.py
@@ -1,25 +1,31 @@
+def total_salary(file_to_open):
+    
+    if len(file_to_open) < 1:
+        file_to_open = "employers_salary.txt"
+
+    employes_salary = 0.0
+    counter = 0
+    try:
+        with open(file_to_open, 'r') as salary:
+            salary.seek(0)
+
+            for line in salary:
+                
+                line = line.rstrip()
+                employe_salary = line.split(',')
+                employes_salary += float(employe_salary[1])
+                counter += 1
+
+    except FileNotFoundError:
+        return f"No such file or directory: {file_to_open}. Try again."
+
+    try:
+        return f"Total salary amount: {employes_salary}, Average salary: {employes_salary / counter:2}"
+
+    except ZeroDivisionError as e:
+        return e
+
+
 file_to_open = input('Please enter name file with informaiton: ')
 
-if len(file_to_open) < 1:
-    file_to_open = "employers_salary.txt"
-
-employes_salary = 0.0
-counter = 0
-try:
-    with open(file_to_open) as salary:
-
-        for line in salary:
-            
-            line = line.rstrip()
-            
-            employe_salary = line.split(',')
-            employes_salary += float(employe_salary[1])
-            counter += 1
-
-except FileNotFoundError:
-    print(f"No such file or directory: {file_to_open}. Try again.")
-
-try:
-    print(f"Total salary amount: {employes_salary}, Average salary: {employes_salary / counter:2}")
-except ZeroDivisionError as e:
-    print(e)
+print(total_salary(file_to_open))
