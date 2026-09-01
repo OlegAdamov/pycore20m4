7db5bdfccd1acebb60ca42def5bbe48698566ef8
Oleg Adamov <103196379+OlegAdamov@users.noreply.github.com>
2026-09-01 12:59:36 +0300

    changed func ans added some try-excepts

--- goit_algo_hw_04_01.py
+++ goit_algo_hw_04_01.py
@@ -2,25 +2,32 @@ def total_salary(file_to_open):
     
     if len(file_to_open) < 1:
         file_to_open = "employers_salary.txt"
+        # return 'It is not a file'
 
     employes_salary = 0.0
     counter = 0
     try:
-        with open(file_to_open, 'r') as salary:
+        with open(file_to_open, 'r', encoding='utf-8') as salary:
             salary.seek(0)
 
             for line in salary:
-                
-                line = line.rstrip()
-                employe_salary = line.split(',')
-                employes_salary += float(employe_salary[1])
-                counter += 1
+
+                try:
+                    line = line.rstrip()
+                    employe_salary = line.split(',')
+                    employes_salary += float(employe_salary[1])
+                    counter += 1
+
+                except IndexError:
+                    print (f"{employe_salary[0]} dees not have a salary")
+                except ValueError:
+                    print (f"{employe_salary[0]} have a incorrect data")
 
     except FileNotFoundError:
         return f"No such file or directory: {file_to_open}. Try again."
 
     try:
-        return f"Total salary amount: {employes_salary}, Average salary: {employes_salary / counter:2}"
+        return employes_salary, (employes_salary / counter)
 
     except ZeroDivisionError as e:
         return e
@@ -28,4 +35,5 @@ def total_salary(file_to_open):
 
 file_to_open = input('Please enter name file with informaiton: ')
 
-print(total_salary(file_to_open))
+total, average = total_salary(file_to_open)
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
